710178ea24c3ec7dac79cce8498bd9eb42038fc1
Natalia Kalashnikova <natashachopper@gmail.com>
2017-11-05 13:08:19 +0100

    Save the information

--- functions.R
+++ functions.R
@@ -50,12 +50,19 @@ return(result)
 #k: a number
 sum_divided_by <- function(x, k) {
   result <- NULL
-  if (is.numeric(x)&&(k)) {
+  if (is.numeric(x)&&is.numeric(k)) {
     result <- (sum(x)) / (k)
     }
    return(result)
 }
 
+my_mean <- function(x) {
+  result <- NULL
+  if (is.numeric(x)) {
+    result <- sum_divided_by(x, length(x))
+  }
+  return(result)
+}
 
 
 # Return a violin plot.
@@ -75,13 +82,14 @@ grouped_violin_plot <- function(d, var, grouping_var) {
   # Create the base ggplot object
   p <- ggplot2::ggplot(d, ggplot2::aes_string(y=var,
                                               x=grouping_var,
-                                              fill=grouping_var)) 
-  p <- p + ggplot2::geom_violin(ggplot2::aes_string(y=var,
-                                           x=grouping_var,
-                                           fill=grouping_var))
+                                              fill=grouping_var)) +
+       ggplot2::geom_violin(ggplot2::aes_string(y=var,
+                                                x=grouping_var,
+                                                fill=grouping_var)) +
+       ggplot2::ggtitle("Iris data") + 
+       ggplot2::scale_fill_brewer(palette="YlGnBu")
   return(p)
 }
-
 
 # Difference in the medians between two groups.
 #
@@ -99,10 +107,67 @@ grouped_violin_plot <- function(d, var, grouping_var) {
 difference_in_medians <- function(d, var, grouping_var, group1, group2) {
   d_1 <- dplyr::filter(d, get(grouping_var) == group1)
   d_2 <- dplyr::filter(d, get(grouping_var) == group2)
-  m1 <- median(d_1)
-  m2 <- median(d_2)
-  result <- m1 - m2
+  result <- median(d_1$var) - median(d_2$var)
   # YOUR CODE HERE: assign the difference in the medians to to the variable 'result'
+  return(result)
+}
+
+# Randomize the order of a column.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d containing the variable to randomize,
+# provided as a string
+#
+# RETURN VALUE:
+# A data frame or tibble exactly the same as d, except with the order of
+# var permuted randomly.
+#
+randomize <- function(d, var) {
+  d[[var]] <- randomize(d[[var]])
+    return(d)
+}
+
+
+
+
+
+# Perform a permutation test for two groups.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of the column in d on which the test statistic will be calculated,
+# provided as a string
+# grouping_var: the name of the column in d which gives the grouping
+# group1: the value of grouping_var corresponding to the first group
+# group2: the value of grouping_var corresponding to the second group
+# statistic: a function yielding a test statistic, which takes as input
+# a data frame, the name of a variable on which to calculate the
+# test statistic, the name of a grouping variable, the value of
+# the grouping variable corresponding to the first group, and
+# the value of the grouping variable corresponding to the second
+# group
+# n_samples: the number of permutation samples to draw (default: 9999)
+#
+# RETURN VALUE:
+#
+# A list containing two elements:
+#
+# - observed: the value of statistic() in d
+# - permuted: a vector containing the values of statistic() under n_samples
+# permutations
+#
+permutation_twogroups <- function(d, var, grouping_var, group1, group2, statistic,
+                                  n_samples=9999) {
+  observed_statistic <- statistic(d, var, grouping_var, group1, group2)
+  permutation_statistics <- rep(0, n_samples)
+  for (i in 1:n_samples) {
+    # YOUR CODE HERE: use randomize(...) to create a permutation and then
+    # fill in the vector permutation_statistics with the
+    # value of statistic(...) for this new permutation
+  }
+  result <- list(observed=observed_statistic,
+                 permuted=permutation_statistics)
   return(result)
 }
 
